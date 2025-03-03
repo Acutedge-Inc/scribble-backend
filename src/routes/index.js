20fe457d1f8a04aa43e59f5c19bf9bb40572638e
tmahara1 <tmahara1@visteon.com>
2025-03-03 23:52:17 +0530

    Added Healthcheck api

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,7 +7,9 @@ const userRoutes = require("./user.js");
 const dashboardRoutes = require("./dashboard.js");
 module.exports = (app) => {
   const baseVersion = nconf.get("BASE_VERSION");
-
+  app.use(`/health`, (req, res) => {
+    res.status(200).json({ message: "OK" });
+  });
   app.use(`/api/${baseVersion}/auth`, authRoutes);
   app.use(`/api/${baseVersion}/visit`, visitRoutes);
   app.use(`/api/${baseVersion}/setting`, settingRoutes);
